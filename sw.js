69e601cca93686b64af2b647d206ec14aad39a72
rebeca018 <rebecasnascimento018@gmail.com>
2024-11-08 14:05:13 -0400

    Arrumando erros

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
 let cacheName = "my-horario-pwa";
-let filesToCache = ["/", "/index.html", "/horario.html", "horarioPe.html",
-                    "/css/style.css", "/css/styleH.css", "/css/stylePE.css","/js/main.js"];
+let filesToCache = ["/", "/index.html", "/horario", "/horarioPe", "/manifest.json",
+                    "/CSS/styles.css", "/CSS/stylesH.css", "/CSS/stylesPE.css","/js/main.js"];
 
 /* inicializando a service worker e fazendo o 
 download do conteúdo da aplicação */
 self.addEventListener("install", (e) => {
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) { return cache.addAll(filesToCache);
-        })
+      caches.open(cacheName).then(function (cache) {
+        return cache.addAll(filesToCache);
+      })
     );
-});
-
-/* disponibilizando o conteudo quando estiver offline*/
-self.addEventListener("fetch", (e) => {
+  });
+  
+  /* disponibilizando o conteudo quando estiver offline */
+  self.addEventListener("fetch", (e) => {
     e.respondWith(
-        caches.match(e.request).then((response) => { 
-            return response || fetch(e.request);
-        })
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      })
     );
-}); 
+  });
+  
+  
